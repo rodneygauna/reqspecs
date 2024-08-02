6b7bfb47b0f89fbc5981fd9d9b609e5e1b89a4ec
Rodney Gauna <rodneygauna@gmail.com>
2024-08-02 11:40:51 -0700

    feat: Add user registration and authentication endpoints
    This commit adds the functionality for user registration and authentication. It includes the following changes:
    - Added new fields (title, departmentID) to the user registration request body
    - Implemented validation for the departmentID field
    - Added logic to check if the department exists before creating a user
    - Updated the registerUser controller to handle the new fields and department logic
    - Added the authUser controller to handle user authentication
    - Created the userProfile controller to retrieve user profile information
    - Implemented the updateUser controller to update user profile data
    
    This commit addresses the need to register and authenticate users in the system, allowing them to access their profile information and update it when necessary.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+
 import User from "../models/userModel.js";
+import Department from "../models/departmentModel.js";
+
 import generateToken from "../utils/generateToken.js";
 
 const SALT = await bcrypt.genSalt(10);
@@ -8,9 +12,17 @@ const SALT = await bcrypt.genSalt(10);
 // @desc    Register a new user
 // @route   POST /api/v1/users/register
 // @access  Public
-const registerUser = asyncHandler(async (req, res) => {
-  const { first_name, last_name, email, password, is_active, user_role } =
-    req.body;
+export const registerUser = asyncHandler(async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    title,
+    departmentID,
+    email,
+    password,
+    is_active,
+    user_role,
+  } = req.body;
 
   // Check if password input is present
   if (!password) {
@@ -25,6 +37,20 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
+  // Check if department exists
+  if (departmentID) {
+    if (!mongoose.isValidObjectId(departmentID)) {
+      res.status(400);
+      throw new Error("Invalid department ID");
+    }
+
+    const departmentExists = await Department.findById(departmentID);
+    if (!departmentExists) {
+      res.status(400);
+      throw new Error("Department not found");
+    }
+  }
+
   // Generate a salt and hash the password
   const hashedPassword = await bcrypt.hash(password, SALT);
 
@@ -32,12 +58,22 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     first_name,
     last_name,
+    title,
+    departmentID,
     email,
     password_hash: hashedPassword,
     is_active: is_active || true,
     user_role: user_role || "user",
   });
 
+  // Add user to department
+  if (departmentID) {
+    const department = await Department.findById(departmentID);
+    department.users.push(user._id);
+    await department.save();
+  }
+
+  // Generate token and send response
   if (user) {
     generateToken(res, user._id);
 
@@ -45,6 +81,8 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       first_name: user.first_name,
       last_name: user.last_name,
+      title: user.title,
+      departmentID: user.departmentID,
       email: user.email,
       is_active: user.is_active,
       user_role: user.user_role,
@@ -58,7 +96,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Auth user & get token
 // @route   POST /api/v1/users/auth
 // @access  Public
-const authUser = asyncHandler(async (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
@@ -72,6 +110,8 @@ const authUser = asyncHandler(async (req, res) => {
       _id: user._id,
       first_name: user.first_name,
       last_name: user.last_name,
+      title: user.title,
+      departmentID: user.departmentID,
       email: user.email,
       is_active: user.is_active,
       user_role: user.user_role,
@@ -86,15 +126,67 @@ const authUser = asyncHandler(async (req, res) => {
 // @desc    Get user profile
 // @route   GET /api/v1/users/profile
 // @access  Private
-const userProfile = asyncHandler(async (req, res) => {
+export const userProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id).select("-password_hash");
 
   if (user) {
-    res.json(user);
+    res.status(201).json(user);
   } else {
     res.status(404);
     throw new Error("User not found");
   }
 });
 
-export { registerUser, authUser, userProfile };
+// @desc    Update user profile
+// @route   PUT /api/v1/users/profile
+// @access  Private
+export const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  // Check if department exists
+  if (req.body.departmentID) {
+    if (!mongoose.isValidObjectId(req.body.departmentID)) {
+      res.status(400);
+      throw new Error("Invalid department ID");
+    }
+
+    const departmentExists = await Department.findById(req.body.departmentID);
+    if (!departmentExists) {
+      res.status(400);
+      throw new Error("Department not found");
+    }
+  }
+
+  // Save updated user data
+  if (user) {
+    const { ...userData } = req.body;
+
+    // Check if departmentID is different
+    if (req.body.departmentID !== user.departmentID) {
+      // Remove user from old department
+      if (user.departmentID) {
+        const oldDepartment = await Department.findById(user.departmentID);
+        oldDepartment.users.pull(user._id);
+        await oldDepartment.save();
+      }
+
+      // Add user to new department
+      if (req.body.departmentID) {
+        const newDepartment = await Department.findById(req.body.departmentID);
+        newDepartment.users.push(user._id);
+        await newDepartment.save();
+      }
+    }
+
+    // Update user data and send response
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      { ...userData },
+      { new: true }
+    );
+    res.status(201).json(updatedUser);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
