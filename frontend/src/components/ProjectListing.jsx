b6c41a5da4385f3859ced84f0cdc927d22b965c5
Rodney Gauna <rodneygauna@gmail.com>
2024-08-16 09:36:07 -0700

    feat: Add epic link to ProjectListing component
    This commit adds the epic link functionality to the ProjectListing component in the frontend. It includes the necessary changes to display the epic link as a clickable link in the project listing. This change enhances the user experience by providing easy access to the epic associated with the project.

--- frontend/src/components/ProjectListing.jsx
+++ frontend/src/components/ProjectListing.jsx
@@ -6,6 +6,7 @@ const ProjectListing = ({ project }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
 
   let problem_description = project.problem_description;
+  let epic_link = project.epic_link;
 
   if (!showFullDescription && problem_description.length > 100) {
     problem_description = problem_description.substring(0, 100) + "...";
@@ -18,7 +19,6 @@ const ProjectListing = ({ project }) => {
           <h3 className="text-xl font-bold">{project.project_name}</h3>
         </div>
         <div className="mb-5">{problem_description}</div>
-
         <button
           onClick={() => setShowFullDescription((prevState) => !prevState)}
           className="text-indigo-500 mb-5 hover:text-indigo-600"
@@ -26,11 +26,21 @@ const ProjectListing = ({ project }) => {
           {showFullDescription ? "Less" : "More"}
         </button>
         <div className="mb-5">
+          <a
+            href={epic_link}
+            target="_blank"
+            rel="noreferrer"
+            className="text-indigo-500 hover:text-indigo-600"
+          >
+            {epic_link}
+          </a>
+        </div>
+        <div className="mb-5">
           <Link
             to={`/project/${project._id}`}
             className="w-full text-white bg-indigo-700 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
-            Read more
+            View project
           </Link>
         </div>
       </div>
