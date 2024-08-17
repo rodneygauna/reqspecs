2bf87d19fbda5f70178d35400e700d8c29e3866c
Rodney Gauna <rodneygauna@gmail.com>
2024-08-17 15:30:18 -0700

    feat: Add departmentLoader function
    This commit adds the departmentLoader function to the frontend. The function is responsible for fetching the details of a single department from the backend API. It takes in the department ID as a parameter and makes a GET request to the appropriate endpoint. This change enhances the application by providing a reusable function for retrieving department data and improves the organization and presentation of department-related functionality.

--- frontend/src/loaders/Loaders.jsx
+++ frontend/src/loaders/Loaders.jsx
@@ -13,3 +13,11 @@ export const companyLoader = async ({ params }) => {
   const data = await response.json();
   return data;
 };
+
+export const departmentLoader = async ({ params }) => {
+  const response = await fetch(
+    `http://localhost:3001/api/v1/departments/${params.id}`
+  );
+  const data = await response.json();
+  return data;
+};
