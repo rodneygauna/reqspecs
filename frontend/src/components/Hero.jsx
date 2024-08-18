81199288d5c7af2e3b0981fb690c27f8ac23f15a
Rodney Gauna <rodneygauna@gmail.com>
2024-08-17 22:40:48 -0700

    feat: Update Hero component with ReqSpecs branding

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Hero = ({
-  title = 'ReqSpecs',
-  subtitle = 'Create, update, and manage your requirements specifications with ease.',
+  title = "ReqSpecs",
+  subtitle = "Create, update, and manage your requirements specifications with ease.",
 }) => {
   return (
-    <section className='bg-indigo-700 py-20 mb-4'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
-        <div className='text-center'>
-          <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
+    <section className="bg-indigo-700 py-20 mb-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
             {title}
           </h1>
-          <p className='my-4 text-xl text-white'>{subtitle}</p>
+          <p className="my-4 text-xl text-white">{subtitle}</p>
         </div>
       </div>
     </section>
