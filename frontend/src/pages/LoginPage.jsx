24ab981824180c7f2f4869a47c2922cb188ed178
Rodney Gauna <rodneygauna@gmail.com>
2024-08-17 22:40:31 -0700

    feat: Improve login page UI and functionality
    This commit updates the LoginPage component to improve the UI and functionality of the login page. It refactors the code to use double quotes for consistency, removes unnecessary imports, and adds event listeners to handle form submission and navigation. These changes enhance the user experience by providing a more visually appealing and user-friendly login page.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import PropTypes from "prop-types";
 
-const LoginPage = ({
-  userLoginSubmit,
-}) => {
+const LoginPage = ({ userLoginSubmit }) => {
   // Form state
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // Navigation
   const navigate = useNavigate();
@@ -18,8 +16,9 @@ const LoginPage = ({
     e.preventDefault();
     userLoginSubmit({ email, password })
       .then(() => {
-        navigate('/');
-        toast.success('Logged in successfully');
+        navigate("/");
+        toast.success("Logged in successfully");
+        window.dispatchEvent(new Event("storage"));
       })
       .catch((error) => {
         toast.error(error.message);
@@ -30,34 +29,81 @@ const LoginPage = ({
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-              <img className="w-8 h-8 mr-2" src="/logo-423x528.png" alt="logo" />
-              ReqSpecs
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                      Sign in to your account
-                  </h1>
-                  <form className="space-y-4 md:space-y-6" onSubmit={submitForm}>
-                      <div>
-                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" value={email} onChange={(e) => setEmail(e.target.value)} />
-                      </div>
-                      <div>
-                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" value={password} onChange={(e) => setPassword(e.target.value)} />
-                      </div>
-                      <div className="flex items-center justify-end">
-                          <a href="#" className="text-sm font-medium text-indigo-600 hover:underline">Forgot password?</a>
-                      </div>
-                      <button type="submit" className="w-full text-white bg-indigo-700 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-                      <p className="text-sm font-light text-gray-500">
-                          Don’t have an account yet? <a href="#" className="font-medium text-indigo-600 hover:underline">Sign up</a>
-                      </p>
-                  </form>
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+        >
+          <img className="w-8 h-8 mr-2" src="/logo-423x528.png" alt="logo" />
+          ReqSpecs
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={submitForm}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex items-center justify-end">
+                <a
+                  href="#"
+                  className="text-sm font-medium text-indigo-600 hover:underline"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-indigo-700 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500">
+                Don’t have an account yet?{" "}
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:underline"
+                >
+                  Sign up
+                </a>
+              </p>
+            </form>
           </div>
+        </div>
       </div>
     </section>
   );
